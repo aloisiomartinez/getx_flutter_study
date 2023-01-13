462fabac6b085a8099fd6f45267ffdc6880fa375
aloisiomartinez <aloisiojacobs36@gmail.com>
2023-01-13 16:53:08 -0300

    reactive with object

---
+++ lib/user_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+import 'package:getx_app_test/user_model.dart';
+
+class UserController extends GetxController {
+  Rx<UserModel> user = UserModel().obs;
+
+  void setUserName(String userName) {
+    // user.update((val) {
+    //   val?.name = userName;
+    // });
+
+    user.value.name = userName;
+    user.refresh();
+  }
+
+  void setUserAge(int userAge) {
+    user.update((val) {
+      val?.age = userAge;
+    });
+  }
+}
